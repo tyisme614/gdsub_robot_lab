a56ca0d1aae9e2fa1fc04d9d7ea0bfd83725c22f
Yuan <yuant614@gmail.com>
2018-06-08 21:24:09 +0800

    replace chinese punctuation to double white spaces

--- RobotLab/utilities.js
+++ RobotLab/utilities.js
@@ -300,9 +300,10 @@ function translate(token, s_block){
     translator.translate(s_block.sentence, target_lang)
         .then(function(results){
             var translation = results[0];
+            translation = translation.replace(/[，|。]/g, '  ');
             s_block.translation = translation;
             stateEmitter.emit(1002, token);
-           // console.log('translation:' + translation);
+
 
         });
 }
