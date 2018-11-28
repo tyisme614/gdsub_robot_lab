c335815507fb1204e7e9e1828ff97cb0242241bc
Yuan <yuant614@gmail.com>
2018-11-28 22:05:20 +0800

    modified rtt translator

--- RobotLab/routes/rttranslator.js
+++ RobotLab/routes/rttranslator.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET real-time translator. */
 router.get('/', function(req, res, next) {
     res.render('rttranslator');
-    next();
+
 });
 
 module.exports = router;
