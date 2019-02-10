040da51e9195620e6dc0e34b1873c6c7d425bcc4
Yuan <yuant614@gmail.com>
2019-02-10 22:37:42 +0800

    test code

--- cloudspeech.js
+++ cloudspeech.js
@@ -26,12 +26,21 @@ const request = {
     config: config,
 };
 
-// Detects speech in the audio file. This creates a recognition job that you
-// can wait for now, or get its result later.
-const [operation] = await client.longRunningRecognize(request);
-// Get a Promise representation of the final result of the job
-const [response] = await operation.promise();
-const transcription = response.results
+
+//try recognize
+client.longRunningRecognize(request, null, (err, response) => {
+    response.results
     .map(result => result.alternatives[0].transcript)
     .join('\n');
-console.log(`Transcription: ${transcription}`);
+    console.log(`Transcription: ${transcription}`);
+});
+
+// Detects speech in the audio file. This creates a recognition job that you
+// can wait for now, or get its result later.
+// const [operation] = client.longRunningRecognize(request);
+// // Get a Promise representation of the final result of the job
+// const [response] = operation.promise();
+// const transcription = response.results
+//     .map(result => result.alternatives[0].transcript)
+//     .join('\n');
+// console.log(`Transcription: ${transcription}`);
